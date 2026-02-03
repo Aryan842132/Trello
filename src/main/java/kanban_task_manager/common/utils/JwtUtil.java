226f901fcea43e4a53dd2ca91ae49e9b6bf8fce7
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-02-03 10:19:25 +0530

    JWT

--- src/main/java/kanban_task_manager/common/utils/JwtUtil.java
+++ src/main/java/kanban_task_manager/common/utils/JwtUtil.java
@@ -16,10 +16,10 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Component
 public class JwtUtil {
 
-    @Value("${jwt.secret:myDefaultSecretKey}")
+    @Value("${jwt.secret}")
     private String SECRET_KEY;
 
-    @Value("${jwt.expiration:86400000}") // 24 hours default
+    @Value("${jwt.expiration}") // 24 hours default
     private Long expirationTime;
 
     public String extractUsername(String token) {
